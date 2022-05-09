4b420706df7fce5b7e33b8a0bd6407a2272458f2
Romina Cattaneo <83126622+romica44@users.noreply.github.com>
2022-05-09 16:38:30 -0300

    Update put.js

--- src/api/routes/movies/put.js
+++ src/api/routes/movies/put.js
@@ -1,16 +1,12 @@
 var express = require('express')
 var router = express.Router()
 var Movie = require('../../sequelize/models/movie')
-var idCheck = require('../../middleware/idCheck')
-var { limitToFields, getFieldsFromModel } = require('../../../helpers')
 
-var fields = getFieldsFromModel(Movie)
 
-router.use(idCheck)
 router.put('/', (req, res, next) => {
-    let fieldsToUpdate = limitToFields(req.body, fields)
+   
     let options = { where: { id: req.body.id } }
-    Movie.update(fieldsToUpdate, options)
+    Movie.update(options)
         .then(response => {
             !response[0] && res.status(404).send("Movie ID not found")
             response[0] && res.status(200).send("Movie updated successfully")
