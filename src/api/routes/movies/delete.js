8713a69f02d09270ff0e5a5afe99e2e1c7e9ac70
Romina Cattaneo <83126622+romica44@users.noreply.github.com>
2022-05-09 16:32:27 -0300

    Update delete.js

--- src/api/routes/movies/delete.js
+++ src/api/routes/movies/delete.js
@@ -1,13 +1,11 @@
 var express = require('express');
-var idCheck = require('../../middleware/idCheck');
 var router = express.Router()
 var Movie = require('../../sequelize/models/movie')
 
-router.use(idCheck)
 router.delete('/', async (req, res, next) => {
     Movie.destroy({ where: { id: req.body.id } })
         .then(response => {
-            !response && res.status(404).send("No entry with that ID was found.")
+            !response && res.status(404).send("Something went wrong")
             response && res.status(200).send("Entry succesfully deleted")
         })
         .catch(err => next(err))
