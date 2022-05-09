b403f167c43a472c72ad690e6f3295be6d07399e
Romina Cattaneo <83126622+romica44@users.noreply.github.com>
2022-05-09 16:37:44 -0300

    Update post.js

--- src/api/routes/movies/post.js
+++ src/api/routes/movies/post.js
@@ -4,14 +4,14 @@ var Movie = require('../../sequelize/models/movie')
 
 router.post('/', (req, res, next) => {
     let { title, image, creationDate, rating, relatedCharacters, genre } = req.body
-    let movieInstance = Movie.build({ title, image, creationDate, rating, relatedCharacters, genre })
+    let movieInstance = Movie.create({ title, image, creationDate, rating, relatedCharacters, genre })
 
     movieInstance
         .validate()
         .then(() => {
             movieInstance
                 .save()
-                .then(() => res.status(200).send("Movie entry uploaded successfully."))
+                .then(() => res.status(200).send("Movie uploaded successfully."))
                 .catch(err => next(err))
         })
         .catch(validationError => res.status(400).send(validationError.message))
